967f7c5d0bbe1999c406ab29e1ff454053f618e4
Mohit Kumar Suman <83830347+mohitkrsuman@users.noreply.github.com>
2021-12-05 22:03:21 +0530

    Add files via upload

---
+++ checkAB.cpp
@@ -0,0 +1,60 @@
+/*
+Check AB
+
+Suppose you have a string, S, made up of only 'a's and 'b's. Write a recursive function that checks if the string was generated using the following rules:
+a. The string begins with an 'a'
+b. Each 'a' is followed by nothing or an 'a' or "bb"
+c. Each "bb" is followed by nothing or an 'a'
+
+If all the rules are followed by the given string, return true otherwise return false.
+
+Input format :
+String S
+
+Output format :
+'true' or 'false'
+
+Constraints :
+1 <= |S| <= 1000
+where |S| represents length of string S.
+
+Sample Input 1 :
+abb
+
+Sample Output 1 :
+true
+
+Sample Input 2 :
+abababa
+
+Sample Output 2 :
+false
+*/
+#include <iostream>
+using namespace std;
+
+bool checkAB(char input[]) {
+   if(input == '\0'){
+       return false;
+   }
+
+   if(input[0] == 'a' && input[1] == 'b' && input[2] == 'b'){
+       return true;
+   }else{
+       return false;
+   }
+   checkAB(input + 3);
+}
+
+int main() {
+    char input[100];
+    bool ans;
+    cin >> input;
+    ans=checkAB(input);
+    if(ans){
+        cout<< "true" << endl;
+    }
+    else{
+        cout<< "false" << endl;
+    }
+}
